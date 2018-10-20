f36db081bc0364c82bb86db14e2f0e52be26bede
Danny <danny@wallou.net>
2018-10-20 15:06:29 -0400

    Added 10 additional images

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var i = Math.floor(Math.random() * 10) + 1;
+    var i = Math.floor(Math.random() * 20) + 1;
     path = 'img/looking-for-mates/' + i + '.jpg';
     $('#bee').attr('src', path);
 });
